afcffbeac465e77c11f4dbe578df926bb7ba5859
@k$hay <akshaynaik5787@gmail.com>
2023-08-19 19:23:44 +0530

    added routers to navigate

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "../pages";
+import { Navbar } from "./";
+
+const Page404 = () => {
+  return <h1>404</h1>
+}
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Router> 
+        
+        <Navbar />
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="*" Component={Page404} />
+        </Routes>
+      </Router>
     </div>
   );
 }
